949f39dc714b627d64c0f5a944b57d4d6fcd145f
Max Donchenko <maxx.donchenko@gmail.com>
2020-04-03 14:40:11 +0300

    Update script description

--- src/userscript.ts
+++ src/userscript.ts
@@ -1,8 +1,4 @@
-const scriptDescription = `
-With this script you will detect which people you follow do not follow you mutually.
-And vice-versa, which your followers don't you follow.
-Source code can be found here: https://github.com/goodwin64/instagram-not-mutual-followers
-`;
+const scriptDescription = "With this script you will detect which people you follow do not follow you mutually. And vice-versa, which your followers don't you follow. Source code can be found here: https://github.com/goodwin64/instagram-not-mutual-followers";
 
 export default {
     id: "instagram-not-mutual-followers",
