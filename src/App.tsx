eb61424ed7f2894fb2bcd770c9410b58917256d5
Max Donchenko <maxx.donchenko@gmail.com>
2020-04-12 20:49:34 +0300

    migrate rendering to React

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {OpenModalButton} from "~src/components/OpenModalButton";
+
+export function App() {
+    return (
+        <OpenModalButton/>
+    )
+}
---
+++ src/operations/addStatisticsButton.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from "~src/App";
+
+export const selectors = {
+  followersLink: '[href*="/followers/"]',
+  followingLink: '[href*="/following/"]',
+  instagramUsername: '.FPmhX',
+  headerControlPanelContainer: '._47KiJ',
+  headerControlButton: '.Fifk5',
+  pageContainer: '.v9tJq.AAaSh.VfzDr',
+  botControlButton: '#bot-control-button',
+};
+
+export function createBotControlButton() {
+  const buttonContainer = document.createElement('div');
+  const headerControlButtonClass = selectors.headerControlButton.replace('.', '');
+  buttonContainer.classList.add(headerControlButtonClass);
+  return buttonContainer;
+}
+
+export function addBotControlButton() {
+  if (document.querySelector(selectors.botControlButton)) {
+    return;
+  }
+  const buttonContainer = createBotControlButton();
+  const controlPanel = document.querySelector(selectors.headerControlPanelContainer);
+  if (!controlPanel) return;
+
+  controlPanel.insertBefore(buttonContainer, controlPanel.firstChild);
+  ReactDOM.render(
+      <App/>,
+      buttonContainer
+  );
+}
+
+export function addStatisticsButton() {
+  setInterval(addBotControlButton, 1000);
+}
