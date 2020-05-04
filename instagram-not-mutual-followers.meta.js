581de8d166f9fe089ce6cabeeff593263699292c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-04 19:43:34 +0000

    Deploy goodwin64/instagram-not-mutual-followers to github.com/goodwin64/instagram-not-mutual-followers.git:gh-pages

--- instagram-not-mutual-followers.meta.js
+++ instagram-not-mutual-followers.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Instagram: not mutual followers
-// @version      0.3.0.2020.4.25.21.1
+// @version      0.3.1.2020.5.4.19.43
 // @description  With this script you will detect which people you follow do not follow you mutually. And vice-versa, which your followers don't you follow.
 // @author       goodwin64
 // @match        *://instagram.com/*
